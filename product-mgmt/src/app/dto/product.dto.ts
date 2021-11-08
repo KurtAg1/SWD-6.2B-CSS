669b3a2fa737ba4bf62de31c22235e1b5412a7f0
Kurt Agius <kurtagius613@gmail.com>
2021-11-08 14:18:48 +0100

    Starting new topic

---
+++ product-mgmt/src/app/dto/product.dto.ts
@@ -0,0 +1,12 @@
+export class Product{
+    private _id: number;
+    private _name: string;
+    private _code: string;
+    private _releaseDate: Date;
+    private _description: string;
+    private _price: number;
+    private _starRating: number;
+    private _imageUrl: string;
+
+    
+}
