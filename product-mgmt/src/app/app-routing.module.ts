6617a896edb6cc5dc446eaf7151d14743a044979
Kurt Agius <kurtagius613@gmail.com>
2021-12-15 12:49:22 +0100

    routing implemented

--- product-mgmt/src/app/app-routing.module.ts
+++ product-mgmt/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'products', component: ProductListComponent},
+  {path: 'products/:id', component: ProductDetailComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- product-mgmt/src/app/app.module.ts
+++ product-mgmt/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConvertToSpacesPipe } from './pipes/convert-to-spaces.pipe';
 import { ProductListComponent } from './product-list/product-list.component';
 import { StarRatingComponent } from './star-rating/star-rating.component';
 import { ProductService } from './services/product.service';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
@@ -15,16 +17,18 @@ import { ProductService } from './services/product.service';
     ProductListComponent,
     ConvertToSpacesPipe,
     StarRatingComponent,
+    HomeComponent,
+    ProductDetailComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [
     ProductService,
   ],
-  bootstrap: [ProductListComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
